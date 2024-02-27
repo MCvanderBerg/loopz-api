10061d38a2687ba76017fa03604d3ca200630655
MCvanderBerg <christiaan26c@gmail.com>
2024-02-28 00:13:12 +0200

    testing normal file

--- app.ts
+++ app.ts
@@ -1,14 +1,27 @@
-import apiRoutes from "./routes/api.routes";
+// import apiRoutes from "./routes/api.routes";
+// const express = require('express');
+//
+// const app = express()
+// const port = 3000;
+//
+//
+// app.use(express.json());
+//
+// app.use('/',apiRoutes)
+//
+// app.listen(process.env.PORT || port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
+
+
 const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const app = express()
-const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
 
-
-app.use(express.json());
-
-app.use('/',apiRoutes)
-
-app.listen(process.env.PORT || port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
