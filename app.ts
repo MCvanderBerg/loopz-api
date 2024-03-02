39d5c079357859ce709db5793c206ae8021feaa1
MCvanderBerg <christiaan26c@gmail.com>
2024-03-02 14:06:31 +0200

    testing old code

--- app.ts
+++ app.ts
@@ -1,35 +1,35 @@
-// import apiRoutes from "./routes/api.routes";
-//  const express = require('express');
-//
-// import router from "./routes/api.routes";
-//
-// const app = express()
-// const port = 3000;
-//
-//
-// app.use(express.json());
-//
+import apiRoutes from "./routes/api.routes";
+ const express = require('express');
+
+import router from "./routes/api.routes";
+
+const app = express()
+const port = 3000;
+
+
+app.use(express.json());
+
 // app.use('/',apiRoutes)
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+router.use('/', (req, res) => {
+    res.status(200).json({message:"its working"})
+})
+
+
 //
-// app.listen(process.env.PORT || port, () => {
-//     console.log(`Server is running on http://localhost:${port}`);
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
+//
+// app.use('/', (req, res) => {
+//     res.status(200).json({ message: 'Hello, World! with request and response 4' });
 // });
-
-// router.use('/', (req: Request, res: Response) => {
-//     res.status(200).json({message:"its working"})
-// })
-
-
-
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use('/', (req, res) => {
-    res.status(200).json({ message: 'Hello, World! with request and response 4' });
-});
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+//
+//
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
