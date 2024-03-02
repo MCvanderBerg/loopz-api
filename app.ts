3f3d9a92728aef4717119758194278ae716f86f5
MCvanderBerg <christiaan26c@gmail.com>
2024-03-02 12:42:50 +0200

    testing

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 // import apiRoutes from "./routes/api.routes";
 //  const express = require('express');
 //
+// import router from "./routes/api.routes";
+//
 // const app = express()
 // const port = 3000;
 //
@@ -13,21 +15,22 @@
 //     console.log(`Server is running on http://localhost:${port}`);
 // });
 
+// router.use('/', (req: Request, res: Response) => {
+//     res.status(200).json({message:"its working"})
+// })
 
-import {Request, Response} from "express";
-import router from "./routes/api.routes";
+
+
+//import {Request, Response} from "express";
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', (req: Request, res: Response) => {
+app.use('/', (req: any, res: any) => {
     res.status(200).json({ message: 'Hello, World!' });
 });
 
-// router.use('/', (req: Request, res: Response) => {
-//     res.status(200).json({message:"its working"})
-// })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
